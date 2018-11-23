afa67f8b312b965a1ab9c0629c43028b22065773
NinjaK222 <44751071+NinjaK222@users.noreply.github.com>
2018-11-23 11:05:58 -0500

    Add files via upload

---
+++ Homework 1.js
@@ -0,0 +1,28 @@
+'use strict'
+
+var Me;
+var LuckyNumber;
+
+Me = "Me";
+LuckyNumber = 8;
+
+alert ("Ready? Okay");
+
+var Counting=3;
+
+while(Counting>0){
+             alert("'"+Counting+"'");Counting--;
+               }
+
+alert ("My favoite number is "+LuckyNumber);
+console.log ("My favoite number is "+LuckyNumber);
+alert ("Oh yeah... I am "+Me);
+
+ if (Me=="Amanda") {
+   alert ("That is correct");
+  console.log (9);
+ }
+
+else{
+ alert ("That is not correct");
+ }
